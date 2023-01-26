08912615f22e4445bfd2e363f398c8bfedaa5884
Inigo <ibagur@gmail.com>
2023-01-26 22:53:33 +0100

    metrics script complete
    - Metrics jupyter notebook
    - Some mat files renamed
    - New shell for BLIP 1e-16

--- BLIP/RL/run_blip_minigrid_f-prior.sh
+++ BLIP/RL/run_blip_minigrid_f-prior.sh
@@ -1,126 +1,84 @@
-python main_rl_minigrid.py --algo ppo --use-gae \
+python main_rl_minigrid.py --algo ppo --use-gae --date 2023-01-26 --date 2023-01-26 \
     --experiment minigrid_5e5_doorkey-wallgap-lavagap-redbluedoor \
     --num-env-steps 500000 --no-cuda \
     --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
     --num-processes 16 --num-steps 128 --num-mini-batch 256 \
     --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-17 --seed 1 --tasks-sequence 0 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
+    --entropy-coef 0.01 --approach blip --F-prior 1e-16 --seed 1 --tasks-sequence 0 && \
+python main_rl_minigrid.py --algo ppo --use-gae --date 2023-01-26 \
     --experiment minigrid_5e5_doorkey-wallgap-lavagap-redbluedoor \
     --num-env-steps 500000 --no-cuda \
     --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
     --num-processes 16 --num-steps 128 --num-mini-batch 256 \
     --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-17 --seed 2 --tasks-sequence 0 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
+    --entropy-coef 0.01 --approach blip --F-prior 1e-16 --seed 2 --tasks-sequence 0 && \
+python main_rl_minigrid.py --algo ppo --use-gae --date 2023-01-26 \
     --experiment minigrid_5e5_doorkey-wallgap-lavagap-redbluedoor \
     --num-env-steps 500000 --no-cuda \
     --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
     --num-processes 16 --num-steps 128 --num-mini-batch 256 \
     --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-17 --seed 3 --tasks-sequence 0 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
-    --experiment minigrid_5e5_doorkey-wallgap-lavagap-redbluedoor-emptyrand \
-    --num-env-steps 500000 --no-cuda \
-    --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
-    --num-processes 16 --num-steps 128 --num-mini-batch 256 \
-    --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-17 --seed 1 --tasks-sequence 2 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
-    --experiment minigrid_5e5_doorkey-wallgap-lavagap-redbluedoor-emptyrand \
-    --num-env-steps 500000 --no-cuda \
-    --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
-    --num-processes 16 --num-steps 128 --num-mini-batch 256 \
-    --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-17 --seed 2 --tasks-sequence 2 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
-    --experiment minigrid_5e5_doorkey-wallgap-lavagap-redbluedoor-emptyrand \
-    --num-env-steps 500000 --no-cuda \
-    --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
-    --num-processes 16 --num-steps 128 --num-mini-batch 256 \
-    --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-17 --seed 3 --tasks-sequence 2 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
-    --experiment minigrid_5e5_doorkey-wallgap-lavagap-redbluedoor \
-    --num-env-steps 500000 --no-cuda \
-    --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
-    --num-processes 16 --num-steps 128 --num-mini-batch 256 \
-    --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-16 --seed 1 --tasks-sequence 0 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
-    --experiment minigrid_5e5_doorkey-wallgap-lavagap-redbluedoor \
-    --num-env-steps 500000 --no-cuda \
-    --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
-    --num-processes 16 --num-steps 128 --num-mini-batch 256 \
-    --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-16 --seed 2 --tasks-sequence 0 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
-    --experiment minigrid_5e5_doorkey-wallgap-lavagap-redbluedoor \
-    --num-env-steps 500000 --no-cuda \
-    --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
-    --num-processes 16 --num-steps 128 --num-mini-batch 256 \
-    --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-16 --seed 3 --tasks-sequence 0 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
+    --entropy-coef 0.01 --approach blip --F-prior 1e-16 --seed 3 --tasks-sequence 0 && \
+python main_rl_minigrid.py --algo ppo --use-gae --date 2023-01-26 \
     --experiment minigrid_5e5_redbluedoor-lavagap-doorkey-wallgap \
     --num-env-steps 500000 --no-cuda \
     --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
     --num-processes 16 --num-steps 128 --num-mini-batch 256 \
     --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-16 --seed 1 --tasks-sequence 1 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
+    --entropy-coef 0.01 --approach blip --F-prior 1e-16 --seed 1 --tasks-sequence 1 && \
+python main_rl_minigrid.py --algo ppo --use-gae --date 2023-01-26 \
     --experiment minigrid_5e5_redbluedoor-lavagap-doorkey-wallgap \
     --num-env-steps 500000 --no-cuda \
     --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
     --num-processes 16 --num-steps 128 --num-mini-batch 256 \
     --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-16 --seed 2 --tasks-sequence 1 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
+    --entropy-coef 0.01 --approach blip --F-prior 1e-16 --seed 2 --tasks-sequence 1 && \
+python main_rl_minigrid.py --algo ppo --use-gae --date 2023-01-26 \
     --experiment minigrid_5e5_redbluedoor-lavagap-doorkey-wallgap \
     --num-env-steps 500000 --no-cuda \
     --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
     --num-processes 16 --num-steps 128 --num-mini-batch 256 \
     --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-16 --seed 3 --tasks-sequence 1 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
+    --entropy-coef 0.01 --approach blip --F-prior 1e-16 --seed 3 --tasks-sequence 1 && \
+python main_rl_minigrid.py --algo ppo --use-gae --date 2023-01-26 \
     --experiment minigrid_5e5_doorkey-wallgap-lavagap-redbluedoor-emptyrand \
     --num-env-steps 500000 --no-cuda \
     --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
     --num-processes 16 --num-steps 128 --num-mini-batch 256 \
     --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-16 --seed 1 --tasks-sequence 2 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
+    --entropy-coef 0.01 --approach blip --F-prior 1e-16 --seed 1 --tasks-sequence 2 && \
+python main_rl_minigrid.py --algo ppo --use-gae --date 2023-01-26 \
     --experiment minigrid_5e5_doorkey-wallgap-lavagap-redbluedoor-emptyrand \
     --num-env-steps 500000 --no-cuda \
     --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
     --num-processes 16 --num-steps 128 --num-mini-batch 256 \
     --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-16 --seed 2 --tasks-sequence 2 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
+    --entropy-coef 0.01 --approach blip --F-prior 1e-16 --seed 2 --tasks-sequence 2 && \
+python main_rl_minigrid.py --algo ppo --use-gae --date 2023-01-26 \
     --experiment minigrid_5e5_doorkey-wallgap-lavagap-redbluedoor-emptyrand \
     --num-env-steps 500000 --no-cuda \
     --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
     --num-processes 16 --num-steps 128 --num-mini-batch 256 \
     --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-16 --seed 3 --tasks-sequence 2 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
+    --entropy-coef 0.01 --approach blip --F-prior 1e-16 --seed 3 --tasks-sequence 2 && \
+python main_rl_minigrid.py --algo ppo --use-gae --date 2023-01-26 \
     --experiment minigrid_5e5_lavagap-doorkey-emptyrand-redbluedoor-wallgap \
     --num-env-steps 500000 --no-cuda \
     --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
     --num-processes 16 --num-steps 128 --num-mini-batch 256 \
     --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-16 --seed 1 --tasks-sequence 3 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
+    --entropy-coef 0.01 --approach blip --F-prior 1e-16 --seed 1 --tasks-sequence 3 && \
+python main_rl_minigrid.py --algo ppo --use-gae --date 2023-01-26 \
     --experiment minigrid_5e5_lavagap-doorkey-emptyrand-redbluedoor-wallgap \
     --num-env-steps 500000 --no-cuda \
     --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
     --num-processes 16 --num-steps 128 --num-mini-batch 256 \
     --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-16 --seed 2 --tasks-sequence 3 && \
-python main_rl_minigrid.py --algo ppo --use-gae \
+    --entropy-coef 0.01 --approach blip --F-prior 1e-16 --seed 2 --tasks-sequence 3 && \
+python main_rl_minigrid.py --algo ppo --use-gae --date 2023-01-26 \
     --experiment minigrid_5e5_lavagap-doorkey-emptyrand-redbluedoor-wallgap \
     --num-env-steps 500000 --no-cuda \
     --lr 2.5e-4 --clip-param 0.2 --value-loss-coef 0.5 \
     --num-processes 16 --num-steps 128 --num-mini-batch 256 \
     --log-interval 10 --eval-interval 10 \
-    --entropy-coef 0.01 --approach blip --F-prior 5e-16 --seed 3 --tasks-sequence 3
+    --entropy-coef 0.01 --approach blip --F-prior 1e-16 --seed 3 --tasks-sequence 3

230242d42b88d1e1f3664d155ee9f0fac828c726
Inigo <ibagur@gmail.com>
2023-02-18 17:59:46 +0100

    Update run_htm_doorkey_cuda_test.sh

--- HTM-PPO/transformer_htm_ppo/run_htm_doorkey_cuda_test.sh
+++ HTM-PPO/transformer_htm_ppo/run_htm_doorkey_cuda_test.sh
@@ -1,2 +1,4 @@
-python train.py --config=./configs/minigrid_htm_doorkey_script_1.yaml  --run-id=minigrid_htm_doorkey_cuda_script_1_1 --seed=1 && \
-python train.py --config=./configs/minigrid_htm_doorkey_script_3.yaml --run-id=minigrid_htm_doorkey_cuda_script_3_1 --seed=1
+python train.py --config=./configs/minigrid_htm_doorkey_script_1.yaml  --run-id=minigrid_htm_doorkey_cuda_script_1_2 --seed=2 && \
+python train.py --config=./configs/minigrid_htm_doorkey_script_1.yaml  --run-id=minigrid_htm_doorkey_cuda_script_1_3 --seed=3 && \
+python train.py --config=./configs/minigrid_htm_doorkey_script_3.yaml --run-id=minigrid_htm_doorkey_cuda_script_3_2 --seed=2 && \
+python train.py --config=./configs/minigrid_htm_doorkey_script_3.yaml --run-id=minigrid_htm_doorkey_cuda_script_3_3 --seed=3

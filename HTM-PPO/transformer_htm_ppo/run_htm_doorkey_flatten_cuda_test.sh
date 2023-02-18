a29f7576645e8d55fe724ef8c7b86156efc3d595
Inigo <ibagur@gmail.com>
2023-02-19 00:51:37 +0100

    Test flatten env with FF input and Attn input

--- HTM-PPO/transformer_htm_ppo/run_htm_doorkey_flatten_cuda_test.sh
+++ HTM-PPO/transformer_htm_ppo/run_htm_doorkey_flatten_cuda_test.sh
@@ -1,3 +1,6 @@
-python train.py --config=./configs/minigrid_htm_doorkey_flatten.yaml --run-id=minigrid_htm_doorkey_flatten_cuda_1 --seed=1 && \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten.yaml --run-id=minigrid_htm_doorkey_flatten_cuda_2 --seed=2 && \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten.yaml --run-id=minigrid_htm_doorkey_flatten_cuda_3 --seed=3
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_a.yaml --run-id=minigrid_htm_doorkey_flatten_ff_cuda_1 --seed=1 && \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_a.yaml --run-id=minigrid_htm_doorkey_flatten_ff_cuda_2 --seed=2 && \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_a.yaml --run-id=minigrid_htm_doorkey_flatten_ff_cuda_3 --seed=3 && \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_b.yaml --run-id=minigrid_htm_doorkey_flatten_attn_cuda_1 --seed=1 && \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_b.yaml --run-id=minigrid_htm_doorkey_flatten_attn_cuda_2 --seed=2 && \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_b.yaml --run-id=minigrid_htm_doorkey_flatten_attn_cuda_3 --seed=3

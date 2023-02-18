989f7529a4a8063f498ab14b44b324ab87cbdb95
Inigo <ibagur@gmail.com>
2023-02-18 19:08:19 +0100

    new shell run test scripts for 3 seeds

---
+++ HTM-PPO/transformer_htm_ppo/run_htm_doorkey_blocks_cuda_test.sh
@@ -0,0 +1,3 @@
+python train.py --config=./configs/minigrid_htm_doorkey_4_blocks.yaml  --run-id=minigrid_htm_doorkey_cuda_4_blocks_1 --seed=1 && \
+python train.py --config=./configs/minigrid_htm_doorkey_4_blocks.yaml  --run-id=minigrid_htm_doorkey_cuda_4_blocks_2 --seed=2 && \
+python train.py --config=./configs/minigrid_htm_doorkey_4_blocks.yaml  --run-id=minigrid_htm_doorkey_cuda_4_blocks_3 --seed=3

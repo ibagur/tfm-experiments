df45d67464ec4f844f745efb43c7cd7c3b6074cc
Inigo <ibagur@gmail.com>
2023-02-19 20:12:33 +0100

    try different lr for AdamW

--- HTM-PPO/transformer_htm_ppo/run_ff_test.sh
+++ HTM-PPO/transformer_htm_ppo/run_ff_test.sh
@@ -1,12 +1,6 @@
-python train.py --config=./configs/minigrid_htm_doorkey_cnn_adam_a.yaml --run-id=minigrid_htm_doorkey_cnn_ff_adam_cuda_1234 --seed=1234 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_cnn_adam_a.yaml --run-id=minigrid_htm_doorkey_cnn_ff_adam_cuda_5678 --seed=5678 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_cnn_adam_a.yaml --run-id=minigrid_htm_doorkey_cnn_ff_adam_cuda_9012 --seed=9012 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_cnn_adamw_a.yaml --run-id=minigrid_htm_doorkey_cnn_ff_adamw_cuda_1234 --seed=1234 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_cnn_adamw_a.yaml --run-id=minigrid_htm_doorkey_cnn_ff_adamw_cuda_5678 --seed=5678 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_cnn_adamw_a.yaml --run-id=minigrid_htm_doorkey_cnn_ff_adamw_cuda_9012 --seed=9012 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_a.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adam_cuda_1234 --seed=1234 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_a.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adam_cuda_5678 --seed=5678 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_a.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adam_cuda_9012 --seed=9012 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_a.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_cuda_1234 --seed=1234 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_a.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_cuda_5678 --seed=5678 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_a.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_cuda_9012 --seed=9012
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_b.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_b_cuda_1234 --seed=1234 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_b.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_b_cuda_5678 --seed=5678 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_b.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_b_cuda_9012 --seed=9012 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_c.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_c_cuda_1234 --seed=1234 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_c.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_c_cuda_5678 --seed=5678 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_c.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_c_cuda_9012 --seed=9012

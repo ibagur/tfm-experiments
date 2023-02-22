a8b183bb1f2c647581c4d731bf5f69d0069c2302
Inigo <ibagur@gmail.com>
2023-02-22 09:19:47 +0100

    test x4 blocks

--- HTM-PPO/transformer_htm_ppo/run_ff_fixup_test.sh
+++ HTM-PPO/transformer_htm_ppo/run_ff_fixup_test.sh
@@ -1,6 +1,6 @@
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adam_tfixup_x2_cuda_1234 --seed=1234 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adam_tfixup_x2_cuda_5678 --seed=5678 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adam_tfixup_x2_cuda_9012 --seed=9012 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_b.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_x2_cuda_1234 --seed=1234 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_b.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_x2_cuda_5678 --seed=5678 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_b.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_x2_cuda_9012 --seed=9012
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adam_tfixup_x4_cuda_1234 --seed=1234 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adam_tfixup_x4_cuda_5678 --seed=5678 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adam_tfixup_x4_cuda_9012 --seed=9012 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_b.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_x4_cuda_1234 --seed=1234 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_b.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_x4_cuda_5678 --seed=5678 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_b.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_x4_cuda_9012 --seed=9012

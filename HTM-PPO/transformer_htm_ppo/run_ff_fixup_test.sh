9b35ab2d0ec1e427f6c635ccfca06f237a6725e3
Inigo <ibagur@gmail.com>
2023-02-25 12:18:21 +0100

    new trainings experiments for hcam tfixup

--- HTM-PPO/transformer_htm_ppo/run_ff_fixup_test.sh
+++ HTM-PPO/transformer_htm_ppo/run_ff_fixup_test.sh
@@ -1,6 +1,9 @@
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adam_tfixup_x4_cuda_1234 --seed=1234 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adam_tfixup_x4_cuda_5678 --seed=5678 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adam_tfixup_x4_cuda_9012 --seed=9012 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_b.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_x4_cuda_1234 --seed=1234 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_b.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_x4_cuda_5678 --seed=5678 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adamw_b.yaml --run-id=minigrid_htm_doorkey_flatten_ff_adamw_x4_cuda_9012 --seed=9012
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup_b.yaml --run-id=minigrid_htm_doorkey_flatten_ff_beta_tfixup_x4_cuda_1234 --seed=1234 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup_b.yaml --run-id=minigrid_htm_doorkey_flatten_ff_beta_tfixup_x4_cuda_5678 --seed=5678 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup_b.yaml --run-id=minigrid_htm_doorkey_flatten_ff_beta_tfixup_x4_cuda_9012 --seed=9012 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup_c.yaml --run-id=minigrid_htm_doorkey_flatten_ff_lr3_tfixup_x4_cuda_1234 --seed=1234 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup_c.yaml --run-id=minigrid_htm_doorkey_flatten_ff_lr3_tfixup_x4_cuda_5678 --seed=5678 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup_c.yaml --run-id=minigrid_htm_doorkey_flatten_ff_lr3_tfixup_x4_cuda_9012 --seed=9012 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup_d.yaml --run-id=minigrid_htm_doorkey_flatten_ff_beta_lr3_tfixup_x4_cuda_1234 --seed=1234 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup_d.yaml --run-id=minigrid_htm_doorkey_flatten_ff_beta_lr3_fixup_x4_cuda_5678 --seed=5678 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup_d.yaml --run-id=minigrid_htm_doorkey_flatten_ff_beta_lr3_tfixup_x4_cuda_9012 --seed=9012

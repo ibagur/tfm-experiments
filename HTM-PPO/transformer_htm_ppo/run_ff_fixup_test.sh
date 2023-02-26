2d826436b788735c52cf7fdc2bfb3a7097148a29
Inigo <ibagur@gmail.com>
2023-02-26 01:11:03 +0100

    Update run_ff_fixup_test.sh

--- HTM-PPO/transformer_htm_ppo/run_ff_fixup_test.sh
+++ HTM-PPO/transformer_htm_ppo/run_ff_fixup_test.sh
@@ -1,3 +1,3 @@
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup_a.yaml --run-id=minigrid_htm_doorkey_flatten_ff_beta1e3_lr25_tfixup_x4_1e6_cuda_1234 --seed=1234 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup_a.yaml --run-id=minigrid_htm_doorkey_flatten_ff_beta1e3_lr25_tfixup_x4_1e6_cuda_5678 --seed=5678 ; \
-python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup_a.yaml --run-id=minigrid_htm_doorkey_flatten_ff_beta1e3_lr25_tfixup_x4_1e6_cuda_9012 --seed=9012
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup_a.yaml --run-id=minigrid_htm_doorkey_flatten_ff_beta1e3_lr25_tfixup_x4_1e6_noreset_cuda_1234 --seed=1234 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup_a.yaml --run-id=minigrid_htm_doorkey_flatten_ff_beta1e3_lr25_tfixup_x4_1e6_noreset_cuda_5678 --seed=5678 ; \
+python train.py --config=./configs/minigrid_htm_doorkey_flatten_adam_b_tfixup_a.yaml --run-id=minigrid_htm_doorkey_flatten_ff_beta1e3_lr25_tfixup_x4_1e6_noreset_cuda_9012 --seed=9012

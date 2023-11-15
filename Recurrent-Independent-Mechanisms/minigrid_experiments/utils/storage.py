81d7985344682fc78275bf6aa7f6e644df464767
Inigo <ibagur@gmail.com>
2023-11-15 10:47:13 +0100

    updated metarims

--- Recurrent-Independent-Mechanisms/minigrid_experiments/utils/storage.py
+++ Recurrent-Independent-Mechanisms/minigrid_experiments/utils/storage.py
@@ -26,16 +26,22 @@ def get_model_dir(model_name):
 def get_status_path(model_dir):
     return os.path.join(model_dir, "status.pt")
 
+def get_status_path_task(model_dir,index):
+    return os.path.join(model_dir, "status", index,".pt")
+
 
 def get_status(model_dir):
     path = get_status_path(model_dir)
     return torch.load(path)
 
 
-def save_status(status, model_dir):
+def save_status(status, model_dir, index):
     path = get_status_path(model_dir)
+    path_task = get_status_path_task(model_dir, index)
     utils.create_folders_if_necessary(path)
+    utils.create_folders_if_necessary(path_task)
     torch.save(status, path)
+    torch.save(status, path_task)
 
 
 def get_vocab(model_dir):
